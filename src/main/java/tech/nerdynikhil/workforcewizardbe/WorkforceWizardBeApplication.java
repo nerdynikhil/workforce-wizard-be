3670653f51996c6f7126eaf371a7e2b40029ab62
Nikhil Barik <36399086+nerdynikhil@users.noreply.github.com>
2023-04-23 15:50:14 +0530

    Initial commit

---
+++ src/main/java/tech/nerdynikhil/workforcewizardbe/WorkforceWizardBeApplication.java
@@ -0,0 +1,13 @@
+package tech.nerdynikhil.workforcewizardbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkforceWizardBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkforceWizardBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/nerdynikhil/workforcewizardbe/WorkforceWizardBeApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.nerdynikhil.workforcewizardbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkforceWizardBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
